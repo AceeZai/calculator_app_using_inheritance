a4f7609cf7e5183bf3ea4c3d749d177acccd3416
Acee Zai Mendez <your_email@example.com>
2026-04-22 23:21:26 +0800

    Made for loop like the first program.

--- student_record.py
+++ student_record.py
@@ -1,3 +1,13 @@
 class StudentRecord:
     def __init__(self, input_filename):
         self.input_filename = input_filename
+
+        for student in input_file:
+            parts = student.strip().split()
+
+            gwa_value = int(parts[-1])
+            student_name = " ".join(parts[:-1])
+
+            if gwa_value > highest_gwa_value:
+                highest_gwa_value = gwa_value
+
