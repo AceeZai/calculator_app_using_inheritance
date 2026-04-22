09479a09c7f87f7d781fa1ec48515c86517c33b2
Acee Zai Mendez <your_email@example.com>
2026-04-23 01:21:14 +0800

    Finalized the whole program

--- life_writer.py
+++ life_writer.py
@@ -4,17 +4,14 @@ class LifeWriter:
 
     def write_lines(self):
         with open(self.filename, "w") as output_file:
-            while true
+            while True:
+                line_input = input("Enter line: ")
+                output_file.write(line_input + "\n")
 
-        output_file = open(self.filename, "w")
+                more_lines = input("Are there more lines y/n? ")
+                if more_lines.lower() == "n":
+                    break
+        print("File writing complete at:", self.filename)
 
-        while True:
-            line_input = input("Enter line: ")
-            output_file.write(line_input + "\n")
-
-            more_lines = input("Are there more lines y/n? ")
-
-            if more_lines.lower() == "n":
-                break
-
-print("Writing file to:", self.filename)
+life_writer = LifeWriter()
+life_writer.write_lines()
