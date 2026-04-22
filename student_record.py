dac50ca6467e85d6423edf6688da555f932b5c48
Acee Zai Mendez <your_email@example.com>
2026-04-22 23:31:24 +0800

    Declared file_input

--- student_record.py
+++ student_record.py
@@ -2,6 +2,12 @@ class StudentRecord:
     def __init__(self, input_filename):
         self.input_filename = input_filename
 
+    def find_highest_gwa(self):
+        input_file = open(self.input_filename, "r")
+
+        highest_student_name = " "
+        highest_gwa_value = -1
+
         for student_line in input_file:
             parts = student_line.strip().split()
 
@@ -12,5 +18,4 @@ class StudentRecord:
                 highest_gwa_value = gwa_value
                 highest_student_name = student_name
 
-                input_file.close()
 
