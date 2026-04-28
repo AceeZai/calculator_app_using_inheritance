75c47db11d94e93b2f49e1208957502a2182908d
Acee Zai Mendez <your_email@example.com>
2026-04-28 23:56:56 +0800

    Added functions menu

--- calculator_app/child_class_calculator.py
+++ calculator_app/child_class_calculator.py
@@ -4,6 +4,14 @@ class Calculator:
     def add_numbers
         self.calculation_history = []
 
+    def display_menu(self):
+        print("n===== SIMPLE CALCULATOR =====")
+        print("1 - Addition")
+        print("2 - Subtraction")
+        print("3 - Multiplication")
+        print("4 - Division")
+        print("=============================")
+
     def subtract_numbers(self, first_number, second_number):
         return first_number - second_number
 
