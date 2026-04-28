ae3c6a1ef03ac551bf95d4cffe983fff9e1a289e
Acee Zai Mendez <your_email@example.com>
2026-04-29 01:17:04 +0800

    Reformat program structure

--- calculator_app/child_class_calculator.py
+++ calculator_app/child_class_calculator.py
@@ -5,6 +5,14 @@ class Calculator:
     def _init_(self):
         self.calculation_history = []
 
+    def get_valid_number(self, input_message):
+        while True:
+            try:
+                user_input = float(input(input_message))
+                return user_input
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+
     def display_menu(self):
         print("\n===== SIMPLE CALCULATOR =====")
         print("1 - Addition")
@@ -14,16 +22,8 @@ class Calculator:
         print("5 - View History")
         print("=============================")
 
-    def get_valid_number(self, input_message):
-        while True:
-            try:
-                user_input = float(input(input_message))
-                return user_input
-            except ValueError:
-                print("Invalid input. Please enter a valid number.")
-
     def save_history(self, first_number, second_number, operation_symbol, result_value):
-        history_entry =  {first_number} {operation_symbol} {second_number} {secpnd_number} = {result_value}""
+        history_entry =  f"{first_number} {operation_symbol} {second_number} = {result_value}""
     def multiply_numbers(self, first_number, second_number):
         return first_number * second_number
 
