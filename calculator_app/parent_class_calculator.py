24bf22841a19bd9c7375b6749cf4482b570aed51
Acee Zai Mendez <your_email@example.com>
2026-04-28 22:06:19 +0800

    Updated calculator app with OOP inheritance and improvements

---
+++ calculator_app/parent_class_calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add_numbers(self, first_number, second_number):
+        return first_number + second_number
+
+    def subtract_numbers(self, first_number, second_number):
+        return first_number - second_number
+
+    def multiply_numbers(self, first_number, second_number):
+        return first_number * second_number
+
+    def divide_numbers(self, first_number, second_number):
+        if second_number == 0:
+            raise ZeroDivisionError("Division by zero is not allowed.")
+        return first_number / second_number
